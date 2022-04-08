5c665e305a5e37ea895874ff481b2d1bfb5654a8
Isaac Kimura <isaac.kimura@iprospect.com>
2022-04-09 01:37:28 +0300

    Initial commit

--- server/index.js
+++ server/index.js
@@ -1,4 +1,26 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postsRouter from "./routes/posts.js";
+
+const app = express();
+
+app.use("/posts", postsRouter);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://Kimura:AySQTBL5m7MLzymq@cluster0.howge.mongodb.net/memories?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening on port ${PORT}`)))
+  .catch((err) => console.log(err));
+
+// mongoose.set("useFindAndModify", false);
